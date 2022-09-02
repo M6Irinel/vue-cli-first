53d36ec6e7b3bdb915f1c7689a6f120a986cd85e
M6Irinel <momoloacairinel666@gmail.com>
2022-09-02 14:26:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="container">
+    <Title />
+    <Hero />
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue';
+import Hero from './components/Hero.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Hero
+  }
+}
+</script>
---
+++ src/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1 class="text-center my-3">La mia prima app con Vue CLI!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+}
+</script>
